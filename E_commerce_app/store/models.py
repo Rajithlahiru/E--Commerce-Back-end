817564dd5bbae23d18a2bf97568d085bc5dc3393
Rajith Lahiru <rajithlahiru7@gmail.com>
2022-02-10 01:52:09 +0530

    Commit added

--- E_commerce_app/store/models.py
+++ E_commerce_app/store/models.py
@@ -1,4 +1,6 @@
 import email
+from turtle import title
+from typing import Collection
 from django.db import models
 
 class Products(models.Model):
@@ -7,10 +9,46 @@ class Products(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection,on_delete=models.PROTECT)
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+    class Order(models.Model):
+        PAYMENT_PENDING = 'P'
+        PAYMENT_COMPLETED = 'C'
+        PAYMENT_FAILED = 'F'
+        
+        PAYMENT_STATUS = [
+            (PAYMENT_PENDING, 'Pending'),
+            (PAYMENT_COMPLETED, 'Completed'),
+            (PAYMENT_FAILED, 'Failed'),
+        ]
+        placed_at = models.DateTimeField(auto_now=True)
+        payment = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_PENDING)
+        Customer = models.ForeignKey(Customer,on_delete=models.PROTECT)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer,on_delete=models.CASCADE, primary_key=True)
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+class Item(models.Model):
+    description = models.TextField()
